16496aecd1c1b68e5b06070cd739466f9d54dc29
Allen Lowe <lallenlowe@gmail.com>
2016-05-05 15:48:16 -0700

    fixed movement bug where you couldn't control in the air.

--- Ape Escape/Assets/Scripts/Motor.cs
+++ Ape Escape/Assets/Scripts/Motor.cs
@@ -8,7 +8,6 @@ public class Motor : MonoBehaviour {
 
 	private Rigidbody2D rb;
 
-	public float horizontalSpeed = 7;
 	public float jumpForce = 7;
 	public float groundedAcceleration = 7f;
 	public float airborneAcceleration = 0.1f;
@@ -20,12 +19,12 @@ public class Motor : MonoBehaviour {
 	}
 
 
-	// Pass 1 or -1 for right and left and 0 to stop instantly
+	// Pass a number between 1 and -1 for right and left and 0 to change nothing
 	public void Horizontal(float direction)
 	{
 		float velocityDelta = direction * groundedAcceleration;
 
-		if (Mathf.Abs(rb.velocity.x) < maxRunSpeed)
+		if ((direction < 0 && rb.velocity.x > -maxRunSpeed) || (direction > 0 && rb.velocity.x < maxRunSpeed))
 			rb.velocity = new Vector2 (rb.velocity.x + velocityDelta, rb.velocity.y);
 
 		
