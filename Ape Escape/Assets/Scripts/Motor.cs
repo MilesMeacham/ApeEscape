9ec051f43ec38038a48bb5efa93d856ec1958a15
Allen Lowe <lallenlowe@gmail.com>
2016-05-05 16:14:32 -0700

    Made the grounded and airborne acceleration/speed systems work the way we wanted

--- Ape Escape/Assets/Scripts/Motor.cs
+++ Ape Escape/Assets/Scripts/Motor.cs
@@ -7,25 +7,39 @@ using System.Collections;
 public class Motor : MonoBehaviour {
 
 	private Rigidbody2D rb;
+	private GroundCheck groundcheck;
 
 	public float jumpForce = 7;
-	public float groundedAcceleration = 7f;
-	public float airborneAcceleration = 0.1f;
-	public float maxRunSpeed = 7;
+	public float groundedAcceleration = 1f;
+	public float airborneAcceleration = 0.5f;
+	public float maxRunSpeed = 9f;
 
 	void Start () 
 	{
 		rb = GetComponent<Rigidbody2D> ();
+		groundcheck = GetComponentInChildren<GroundCheck> ();
 	}
 
 
 	// Pass a number between 1 and -1 for right and left and 0 to change nothing
 	public void Horizontal(float direction)
 	{
-		float velocityDelta = direction * groundedAcceleration;
+		float velocityDelta;
+		if(groundcheck.grounded)
+			velocityDelta = direction * groundedAcceleration;
+		else
+			velocityDelta = direction * airborneAcceleration;
+		
 
 		if ((direction < 0 && rb.velocity.x > -maxRunSpeed) || (direction > 0 && rb.velocity.x < maxRunSpeed))
-			rb.velocity = new Vector2 (rb.velocity.x + velocityDelta, rb.velocity.y);
+		{
+			float newVelocity = rb.velocity.x + velocityDelta;
+			if (newVelocity > maxRunSpeed)
+				newVelocity = maxRunSpeed;
+			else if (newVelocity < -maxRunSpeed)
+				newVelocity = -maxRunSpeed;
+			rb.velocity = new Vector2 (newVelocity, rb.velocity.y);
+		}
 
 		
 
