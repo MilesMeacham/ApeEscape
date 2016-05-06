6ca5dde838f688a6487bb333881a799f8c4c33ac
Allen Lowe <lallenlowe@gmail.com>
2016-05-05 17:23:08 -0700

    added preliminary support for max run speed to account for gamepad joystick position

--- Ape Escape/Assets/Scripts/Motor.cs
+++ Ape Escape/Assets/Scripts/Motor.cs
@@ -9,7 +9,7 @@ public class Motor : MonoBehaviour {
 	private Rigidbody2D rb;
 	private GroundCheck groundcheck;
 
-	public float jumpForce = 7;
+	public float jumpForce = 9;
 	public float groundedAcceleration = 1f;
 	public float airborneAcceleration = 0.5f;
 	public float maxRunSpeed = 9f;
@@ -24,20 +24,23 @@ public class Motor : MonoBehaviour {
 	// Pass a number between 1 and -1 for right and left and 0 to change nothing
 	public void Horizontal(float direction)
 	{
+		float maxSpeedAdjusted;
 		float velocityDelta;
+
+		maxSpeedAdjusted = maxRunSpeed * direction;
 		if(groundcheck.grounded)
 			velocityDelta = direction * groundedAcceleration;
 		else
 			velocityDelta = direction * airborneAcceleration;
 		
-
-		if ((direction < 0 && rb.velocity.x > -maxRunSpeed) || (direction > 0 && rb.velocity.x < maxRunSpeed))
-		{
-			float newVelocity = rb.velocity.x + velocityDelta;
-			if (newVelocity > maxRunSpeed)
-				newVelocity = maxRunSpeed;
-			else if (newVelocity < -maxRunSpeed)
-				newVelocity = -maxRunSpeed;
+		float newVelocity = rb.velocity.x + velocityDelta;
+		if (direction < 0 && rb.velocity.x > maxSpeedAdjusted) {
+			if (newVelocity < maxSpeedAdjusted)
+				newVelocity = maxSpeedAdjusted;
+			rb.velocity = new Vector2 (newVelocity, rb.velocity.y);
+		} else if (direction > 0 && rb.velocity.x < maxSpeedAdjusted) {
+			if (newVelocity > maxSpeedAdjusted)
+				newVelocity = maxSpeedAdjusted;
 			rb.velocity = new Vector2 (newVelocity, rb.velocity.y);
 		}
 
