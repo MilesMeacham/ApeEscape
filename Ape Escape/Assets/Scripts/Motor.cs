6b73dd349c29f907ccf145c085a20290eed90b91
Miles Meacham <littlemeacham@gmail.com>
2016-05-05 21:45:17 -0600

    Added a flip. Doing so need RightHorizontal and LeftHorizontal instead of just Horizontal. Need to remove unneccessary code.

--- Ape Escape/Assets/Scripts/Motor.cs
+++ Ape Escape/Assets/Scripts/Motor.cs
@@ -1,4 +1,4 @@
-﻿// AUTHOR: Miles
+﻿// AUTHOR: Miles and Allen
 // DESCRIPTION: This handles the movement of the characters (Player and Enemies)
 
 using UnityEngine;
@@ -8,43 +8,93 @@ public class Motor : MonoBehaviour {
 
 	private Rigidbody2D rb;
 	private GroundCheck groundcheck;
+	private float direction = 1;
+	private Vector3 scale;
 
 	public float jumpForce = 9;
 	public float groundedAcceleration = 1f;
 	public float airborneAcceleration = 0.5f;
 	public float maxRunSpeed = 9f;
 
+	public bool facingRight = true;
+
 	void Start () 
 	{
 		rb = GetComponent<Rigidbody2D> ();
 		groundcheck = GetComponentInChildren<GroundCheck> ();
+		scale = transform.localScale;
 	}
 
 
-	// Pass a number between 1 and -1 for right and left and 0 to change nothing
-	public void Horizontal(float direction)
+
+	public void RightHorizontal(float speed)
 	{
+		if (!facingRight)
+			Flip ();
+
 		float maxSpeedAdjusted;
 		float velocityDelta;
 
 		maxSpeedAdjusted = maxRunSpeed * direction;
+
 		if(groundcheck.grounded)
 			velocityDelta = direction * groundedAcceleration;
 		else
 			velocityDelta = direction * airborneAcceleration;
 		
 		float newVelocity = rb.velocity.x + velocityDelta;
-		if (direction < 0 && rb.velocity.x > maxSpeedAdjusted) {
+
+		if (direction < 0 && rb.velocity.x > maxSpeedAdjusted) 
+		{
 			if (newVelocity < maxSpeedAdjusted)
 				newVelocity = maxSpeedAdjusted;
+			
 			rb.velocity = new Vector2 (newVelocity, rb.velocity.y);
-		} else if (direction > 0 && rb.velocity.x < maxSpeedAdjusted) {
+		} 
+		else if (direction > 0 && rb.velocity.x < maxSpeedAdjusted) 
+		{
 			if (newVelocity > maxSpeedAdjusted)
 				newVelocity = maxSpeedAdjusted;
+			
 			rb.velocity = new Vector2 (newVelocity, rb.velocity.y);
 		}
+			
 
-		
+		print (rb.velocity);
+	}
+
+
+	public void LeftHorizontal(float speed)
+	{
+		if (facingRight)
+			Flip ();
+
+		float maxSpeedAdjusted;
+		float velocityDelta;
+
+		maxSpeedAdjusted = maxRunSpeed * direction;
+
+		if(groundcheck.grounded)
+			velocityDelta = direction * groundedAcceleration;
+		else
+			velocityDelta = direction * airborneAcceleration;
+
+		float newVelocity = rb.velocity.x + velocityDelta;
+
+		if (direction < 0 && rb.velocity.x > maxSpeedAdjusted) 
+		{
+			if (newVelocity < maxSpeedAdjusted)
+				newVelocity = maxSpeedAdjusted;
+
+			rb.velocity = new Vector2 (newVelocity, rb.velocity.y);
+		} 
+		else if (direction > 0 && rb.velocity.x < maxSpeedAdjusted) 
+		{
+			if (newVelocity > maxSpeedAdjusted)
+				newVelocity = maxSpeedAdjusted;
+
+			rb.velocity = new Vector2 (newVelocity, rb.velocity.y);
+		}
 
 
 		print (rb.velocity);
@@ -58,6 +108,18 @@ public class Motor : MonoBehaviour {
 
 		rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
 		//rb.AddForce (new Vector2 (rb.velocity.x, jumpForce));
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+
+		// Reverses the speed;
+		direction *= -1;
+
+		scale.x *= -1;
+		transform.localScale = scale;
+
 	}
 
 
