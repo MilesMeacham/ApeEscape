f07a6ac490be8764680de9d1ee4f1fab425b90fe
Allen Lowe <lallenlowe@gmail.com>
2016-05-06 18:32:27 -0700

    added the first test rope (worst rope ever, will have to replace with a 2D solution)

---
+++ Ape Escape/Assets/Scripts/RestrictTo2D.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestrictTo2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//prevents the object from moving and rotating in a way unfit for 2D
+
+	void FixedUpdate () {
+		//transform.localPosition.z = 0;
+		transform.eulerAngles = new Vector3 (0, 0);
+	}
+}
