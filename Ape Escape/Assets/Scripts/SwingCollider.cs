11febce9dfbd523457041b610a6875f98d31a089
Miles Meacham <littlemeacham@gmail.com>
2016-05-07 14:44:57 -0600

    Swinging Prototype is working. Crazy! It sucks compared to what we want but it's a start.

---
+++ Ape Escape/Assets/Scripts/SwingCollider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingCollider : MonoBehaviour {
+
+	public bool attach = false;
+	public Swing swing;
+
+	void OnTriggerStay2D (Collider2D collider)
+	{
+		if (collider.gameObject.layer == 9 && attach == false) 
+		{
+			
+			swing.hinge.connectedBody = collider.gameObject.GetComponent<Rigidbody2D> ();
+			swing.baseLink.connectedBody = collider.transform.parent.GetComponent<Rigidbody2D> ();
+			attach = true;
+		}
+	}
+
+}
