23c09f4368322fc326bb49a8b1680ce1f5d9d44a
Miles Meacham <littlemeacham@gmail.com>
2016-05-14 09:47:15 -0600

    Checkpoints and deathzones are working

---
+++ Ape Escape/Assets/TriggerDetection.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDetection : MonoBehaviour {
+
+	private GameManager gameManager;
+
+	void Start ()
+	{
+		// I Can use this method since there is only one gameManager
+		// FYI it is not always great to use this method. It has to look through many gameobjects when the scene gets big
+		gameManager = FindObjectOfType<GameManager> ();
+
+	}
+
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		if (collider.gameObject.layer == 10)
+			gameManager.RespawnPlayer ();
+
+
+	}
+
+}
