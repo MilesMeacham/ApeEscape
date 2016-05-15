316d426468729f78bfa551f8e5d85246468a9b83
Miles Meacham <littlemeacham@gmail.com>
2016-05-14 22:55:32 -0600

    Mud Splash added. We will want to make particle systems reusable instead of instantiating them everytime. Will do that later.

---
+++ Ape Escape/Assets/Scripts/DestroyWhenDonePlaying.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyWhenDonePlaying : MonoBehaviour {
+
+	private ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ps = GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (ps.isPlaying == false)
+			Destroy (gameObject);
+	}
+}
