6664595a2414c5f455b1073daa9b66cf334dcb24
Miles Meacham <littlemeacham@gmail.com>
2016-05-15 14:59:43 -0600

    added buoyancy to the water.

---
+++ Ape Escape/Assets/Scripts/WaterWeight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterWeight : MonoBehaviour {
+
+	// This weight makes it so the box sits flat, not at an angle
+	public GameObject waterWeight;
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+
+		if (collider.gameObject.tag == "Water")
+			waterWeight.SetActive (true);
+
+	}
+
+	void OnTriggerExit2D (Collider2D collider)
+	{
+
+		if (collider.gameObject.tag == "Water")
+			waterWeight.SetActive (true);
+
+	}
+}
