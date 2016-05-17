c55c2c4a8f66b606e41e678425d2b4578235e478
Allen Lowe <lallenlowe@gmail.com>
2016-05-16 21:06:53 -0700

    Well, I think this is close to as good as rotation is going to get with such a flawed method. I will have to start from scratch.

--- Ape Escape/Assets/Scripts/RotationCheck.cs
+++ Ape Escape/Assets/Scripts/RotationCheck.cs
@@ -8,17 +8,26 @@ using System.Collections.Generic;
 public class RotationCheck : MonoBehaviour {
 
 	private GroundCheck groundcheck;
-	public float rotateDelay = 0.3f;
+	public float rotateSpeed = 1f;
 	public bool collided = false;
-	private bool dontRotate = false;
+	private bool doneRotating = true;
 
-	[HideInInspector]
-	public List<Collider2D> colliders = new List<Collider2D>();
+	private float target_angle = 0f;
+
 
 	void Start()
 	{
 		groundcheck = transform.parent.gameObject.GetComponentInChildren<GroundCheck> ();
-		colliders = new List<Collider2D>();
+	}
+
+	float reverseAngle(float angle)
+	{
+		float new_angle = 360 - angle;
+		if (new_angle == 360) {
+			new_angle = 0;
+		}
+
+		return new_angle;
 	}
 
 	void OnTriggerEnter2D (Collider2D collider)
@@ -26,40 +35,19 @@ public class RotationCheck : MonoBehaviour {
 		// Layer 8 is the ground Layer
 		if (collider.gameObject.layer == 8) {
 			collided = true;
-			if (!colliders.Contains (collider)) {
-				colliders.Add (collider);
-			}
+			OnTriggerStay2D (collider);
 		}
-	}
-
-	IEnumerator RotateDelayCO()
-	{
-		dontRotate = true;
-
-		yield return new WaitForSeconds(rotateDelay);
-
-		dontRotate = false;
-
 	}
 
 	void OnTriggerStay2D(Collider2D collider)
 	{
 		if (collider.gameObject.layer == 8) {
 			collided = true;
-			if (dontRotate == false) {
-				var original_euler = transform.parent.transform.rotation.eulerAngles;
-				var new_euler = collider.transform.rotation.eulerAngles;
-				var euler = new_euler;
-				foreach (Collider2D checking_collider in colliders) {
-					var checking_euler = checking_collider.transform.rotation.eulerAngles;
-					if (checking_euler.z == 0) {
-						euler = checking_euler;
-					}
-				}
-				if (original_euler.z != euler.z && euler.z != 0) {
-					StartCoroutine (RotateDelayCO ());
-					transform.parent.transform.rotation = Quaternion.Euler (0, 0, -euler.z);
-				}
+			var original_euler = transform.parent.transform.rotation.eulerAngles;
+			var new_euler = collider.transform.rotation.eulerAngles;
+
+			if (original_euler.z != new_euler.z) {
+				target_angle = new_euler.z;
 			}
 		}
 	}
@@ -69,28 +57,48 @@ public class RotationCheck : MonoBehaviour {
 		// Layer 8 is the ground Layer
 		if (collider.gameObject.layer == 8) {
 			collided = false;
-			if(colliders.Contains(collider))
-				colliders.Remove(collider);
-			if (dontRotate) {
-				StartCoroutine (ExitDelayCO ());
-			} else if(!groundcheck.grounded && !collided) {
-				transform.parent.transform.rotation = Quaternion.Euler (0, 0, 0);
+			if(!groundcheck.grounded && !collided) {
+				target_angle = 0;
 			}
 		}
 	}
 
-	IEnumerator ExitDelayCO()
+	void FixedUpdate()
 	{
-		while (dontRotate == false) {
-			yield return null;
+		var original_angle = transform.parent.transform.rotation.eulerAngles.z;
+		//string printed = "original_angle: " + original_angle + " target_angle: " + target_angle;
+		//print (printed);
+		if (original_angle > target_angle) {
+			if (Mathf.Abs(original_angle - target_angle) < rotateSpeed) {
+				transform.parent.transform.rotation = Quaternion.Euler (0, 0, target_angle);
+				doneRotating = true;
+			} else if (Mathf.Abs (original_angle - target_angle) > 180) {
+				transform.parent.transform.rotation = Quaternion.Euler (0, 0, original_angle + rotateSpeed);
+				doneRotating = false;
+			} else {
+				transform.parent.transform.rotation = Quaternion.Euler (0, 0, original_angle - rotateSpeed);
+				doneRotating = false;
+			}
+		} else if (original_angle < target_angle) {
+			if (Mathf.Abs(target_angle - original_angle) < rotateSpeed) {
+				transform.parent.transform.rotation = Quaternion.Euler (0, 0, target_angle);
+				doneRotating = true;
+			} else if (Mathf.Abs (target_angle - original_angle) > 180) {
+				transform.parent.transform.rotation = Quaternion.Euler (0, 0, original_angle - rotateSpeed);
+				doneRotating = false;
+			} else {
+				transform.parent.transform.rotation = Quaternion.Euler (0, 0, original_angle + rotateSpeed);
+				doneRotating = false;
+			}
+		} else {
+			doneRotating = true;
 		}
-		if(!groundcheck.grounded && !collided)
-			transform.parent.transform.rotation = Quaternion.Euler (0, 0, 0);
 	}
 
 	public void Reset()
 	{
-		colliders = new List<Collider2D>();
-		dontRotate = false;
+		collided = false;
+		target_angle = 0;
+		doneRotating = true;
 	}
 }
